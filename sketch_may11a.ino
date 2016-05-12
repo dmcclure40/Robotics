47a284d93cd680433355da1fff4bca059a613158
MonumentToAllYourSins <adhe2495@colorado.edu>
2016-05-12 11:30:22 -0600

    Odometry works and added comments

--- sketch_may11a.ino
+++ sketch_may11a.ino
@@ -1,31 +1,27 @@
 #include <sparki.h>
-  
+
+  // Global variables
   float xI = 0;
   float yI = 0;
   float tI = 0;
-  float tnot = 0;
-
 
 void setup() {
   // put your setup code here, to run once:
-  
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // Record start time of loop
   unsigned long timeStart = millis();
   
-  float xR = 0;
-  float tR = 0;
-  float dim = .085; //        m
-  float speed = .02717; //    m/s
-  int threshold = 500;
-  float xItemp = 0;
-  float yItemp = 0;
-  float tItemp = 0;
-  
+  float xR = 0;             // m/s
+  float tR = 0;             // rads/sec
+  float dim = .084;         // m
+  float speed = .02717;     // m/s
+  float dt = 0.1;           // seconds
  
+  int threshold = 500;
   int lineLeft   = sparki.lineLeft();   // measure the left IR sensor
   int lineCenter = sparki.lineCenter(); // measure the center IR sensor
   int lineRight  = sparki.lineRight();  // measure the right IR sensor
@@ -33,72 +29,49 @@ void loop() {
   if ( lineLeft < threshold ) // if line is below left line sensor
   {  
     sparki.moveLeft(); // turn left
-    xR = speed/2;
-    tR = -1*(speed/dim);
+    // Update position and speed of the Robot's coordinate frame
+    xR = 0;
+    tR = -2*(speed/dim);
   }
  
   if ( lineRight < threshold ) // if line is below right line sensor
   {  
     sparki.moveRight(); // turn right
-    xR = speed/2;
-    tR = speed/dim;
+    // Update position and speed of the Robot's coordinate frame
+    xR = 0;
+    tR = 2*speed/dim;
   }
  
   // if the center line sensor is the only one reading a line
   if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) )
   {
     sparki.moveForward(); // move forward
+    // Update position and speed of the Robot's coordinate frame
     xR = speed;
     tR = 0;
   }  
 
-  /*
-  sparki.clearLCD(); // wipe the screen
- 
-  sparki.print("Line Left: "); // show left line sensor on screen
-  sparki.println(lineLeft);
- 
-  sparki.print("Line Center: "); // show center line sensor on screen
-  sparki.println(lineCenter);
- 
-  sparki.print("Line Right: "); // show right line sensor on screen
-  sparki.println(lineRight);
- 
-  sparki.updateLCD(); // display all of the information written to the screen
-  */
-  float dt = 0.1; //seconds
+  // Update Inertial Frame position based on odometry, using global variables
+  xI = xI + cos(tI)*xR*dt; // m
+  yI = yI + sin(tI)*xR*dt; // m
+  tI = tI + tR*dt;         // rad
 
-  xItemp = xI + cos(tItemp)*xR*dt; //cm
-  yItemp = yI + sin(tItemp)*xR*dt; //cm
-  tItemp = tI + tR*dt;
-
-  xI = xItemp;
-  yI = yItemp;
-  tI = tItemp;
+  // Print position and orientation to the screen
+  sparki.clearLCD(); 
   
-  
-  //xI = xI + cos(tnot)*xR*dt;
-  //yI = yI + sin(tnot)*xR*dt;
-  //tI = tI + tR*dt;
-  
-  //tnot = atan2(yItemp, xItemp);
-
-  sparki.clearLCD(); // wipe the screen
- 
-  sparki.print("xI: "); // show left line sensor on screen
+  sparki.print("xI: "); 
   sparki.println(xI, 4);
  
-  sparki.print("yI: "); // show center line sensor on screen
+  sparki.print("yI: "); 
   sparki.println(yI, 4);
  
-  sparki.print("thetaI(deg): "); // show right line sensor on screen
-  sparki.println(tItemp*(180/3.14), 4);
+  sparki.print("thetaI(deg): "); 
+  sparki.println(tI*(180/3.14), 2); // converted to degrees for ease of reading
  
   sparki.updateLCD();
 
+  // Record end timer and then institue the 100ms delay
   unsigned long timeEnd = millis();
   delay(100 - (timeEnd - timeStart)); // wait 0.1 seconds
-
-  
 
 }
