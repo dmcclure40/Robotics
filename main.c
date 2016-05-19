0ead673f495af8b451780a94f3426157e3ab7a26
Ninjahamster611 <ninjahamster94@gmail.com>
2016-05-19 10:51:08 -0600

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,11 @@
 #include <stdio.h>
 
 void createCost() {
-    int x = 2;
-    int y = 2;
+    int x = 3;
+    int y = 3;
     int n = x*y;
-    int map[2][2] = {{1, 1}, {0, 1}};
-    int cost[4][4] = {0};
+    int map[3][3] = {{1, 0, 1}, {1, 0, 1}, {1, 1, 1}};
+    int cost[9][9] = {0};
     for (int i = 0; i < x; i++) {
         for (int j = 0; j < y; j++) {
             // Right neighbors
@@ -21,54 +21,44 @@ void createCost() {
                 if (map[i][j] && map[i+1][j]) {
                     cost[(x*j)+i+1][(x*j)+i] = 1;
                 }
-            }
-            for (int j = 0; j < 4; j++) {
-                for (int i = 0; i < 4; i++) {
-                    printf("%d", cost[i][j]);
+                if (map[i][j] && (map[i+1][j] == 0)) {
+                    cost[(x*j)+i+1][(x*j)+i] = 100;
                 }
-                printf("\n");
             }
-            printf("\n");
             // Left neighbors
             if (i > 0) {
                 if (map[i][j] && map[i-1][j]) {
                     cost[(x*j)+i-1][(x*j)+i] = 1;
                 }
-            }
-            for (int j = 0; j < 4; j++) {
-                for (int i = 0; i < 4; i++) {
-                    printf("%d", cost[i][j]);
+                if (map[i][j] && (map[i-1][j] == 0)) {
+                    cost[(x*j)+i-1][(x*j)+i] = 100;
                 }
-                printf("\n");
             }
-            printf("\n");
             // Up neighbors
             if (j > 0) {
                 if (map[i][j] && map[i][j-1]) {
                     cost[(x*j)+i-x][(x*j)+i] = 1;
                 }
-            }
-            for (int j = 0; j < 4; j++) {
-                for (int i = 0; i < 4; i++) {
-                    printf("%d", cost[i][j]);
+                if (map[i][j] && (map[i][j-1] == 0)) {
+                    cost[(x*j)+i-x][(x*j)+i] = 100;
                 }
-                printf("\n");
             }
-            printf("\n");
             // Down neighbors
             if (j < y-1) {
                 if (map[i][j] && map[i][j+1]) {
                     cost[(x*j)+i+x][(x*j)+i] = 1;
                 }
-            }
-            for (int j = 0; j < 4; j++) {
-                for (int i = 0; i < 4; i++) {
-                    printf("%d", cost[i][j]);
+                if (map[i][j] && (map[i][j+1] == 0)) {
+                    cost[(x*j)+i+x][(x*j)+i] = 100;
                 }
-                printf("\n");
             }
-            printf("\n");
         }
+    }
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            printf("%d,", cost[i][j]);
+        }
+        printf("\n");
     }
 }
 
